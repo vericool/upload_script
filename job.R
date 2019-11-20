7afb2aac7b7670e63ba4498ea223f3f9203aaa7a
vericool <57604122+vericool@users.noreply.github.com>
2019-11-20 11:32:13 -0500

    Update job.R

--- job.R
+++ job.R
@@ -4,5 +4,5 @@ library(cronR)
 
 cmd <- cron_rscript("/home/ubuntu/node_api/upload_script/code.R")
 cmd
-cron_add(cmd, frequency = 'daily', id = 'job2', description = 'Customers', at = '16:30')
+cron_add(cmd, frequency = 'daily', id = 'job2', description = 'Customers', at = '05:00')
 
